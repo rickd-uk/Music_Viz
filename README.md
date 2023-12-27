e2508ddf65b6e8429e108e99c56da3eb4f032bb5
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-12-27 19:51:26 +0900

    Music Visualizer 46:35 - Display sampleRate, sampleSize & channels info

---
+++ README.md
@@ -0,0 +1,7 @@
+# Music Visualizer
+
+### Ref:
+- [Raylib Home](https://www.raylib.com/)
+- [Veritasium Video on FFT](https://www.youtube.com/watch?v=nmgFG7PUHfo)
+
+
--- main.c
+++ main.c
@@ -1,22 +1,25 @@
-#include <stdio.h>
 #include <raylib.h>
+#include <stdio.h>
 
-void callback(void *bufferData, unsigned int frames)
-{
-  (void) bufferData;
-  printf("callback(frames = %u)\n", frames);
+void callback(void *bufferData, unsigned int frames) {
+  (void)bufferData;
+  /* printf("callback(frames = %u)\n", frames); */
 }
 
-int main (void) {
+int main(void) {
   InitWindow(800, 600, "musicviz");
   SetTargetFPS(60);
   InitAudioDevice();
 
   Music music = LoadMusicStream("Starship.ogg");
+  printf("music.frameCount = %u\n", music.frameCount);
 
   PlayMusicStream(music);
   SetMusicVolume(music, 0.4f);
   AttachAudioStreamProcessor(music.stream, callback);
+  printf("music.stream.sampleRate = %u\n", music.stream.sampleRate);
+  printf("music.stream.sampleSize = %u\n", music.stream.sampleSize);
+  printf("music.stream.channels = %u\n", music.stream.channels);
   while (!WindowShouldClose()) {
     UpdateMusicStream(music);
 
@@ -27,7 +30,7 @@ int main (void) {
         ResumeMusicStream(music);
       }
     }
-      BeginDrawing();
+    BeginDrawing();
     ClearBackground(BLUE);
     EndDrawing();
   }
