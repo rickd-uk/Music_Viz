141ab4a5ae1f309c23015ad9750ea610d1f8dc86
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-12-27 18:11:56 +0900

    Music Visualizer 38:00 - Get frames buffer via callback

--- main.c
+++ main.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <raylib.h>
 
+void callback(void *bufferData, unsigned int frames)
+{
+  (void) bufferData;
+  printf("callback(frames = %u)\n", frames);
+}
+
 int main (void) {
   InitWindow(800, 600, "musicviz");
+  SetTargetFPS(60);
   InitAudioDevice();
 
   Music music = LoadMusicStream("Starship.ogg");
 
-    PlayMusicStream(music);
-    SetMusicVolume(music, 0.4f);
+  PlayMusicStream(music);
+  SetMusicVolume(music, 0.4f);
+  AttachAudioStreamProcessor(music.stream, callback);
   while (!WindowShouldClose()) {
     UpdateMusicStream(music);
 
@@ -19,9 +27,11 @@ int main (void) {
         ResumeMusicStream(music);
       }
     }
-    BeginDrawing();
+      BeginDrawing();
     ClearBackground(BLUE);
     EndDrawing();
   }
   return 0;
 }
+
+// https://www.youtube.com/watch?v=Xdbk1Pr5WXU&list=PLpM-Dvs8t0Vak1rrE2NJn8XYEJ5M7-BqT&index=1
