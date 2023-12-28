86e2668e976135b5ded2857de514de0fbf41c040
Rick D <78909973+rickd-uk@users.noreply.github.com>
2023-12-28 21:31:16 +0900

    Music Visualizer 2:10 - Implement faster fourier transform

--- fft.c
+++ fft.c
@@ -8,9 +8,7 @@ int main() {
   pi = atan2f(1, 1) * 4;
   /* printf("%.10f\n", pi); */
   size_t n = 8;
-
   float in[n];
-
   float complex out[n];
 
   for (size_t i = 0; i < n; ++i) {
@@ -18,8 +16,30 @@ int main() {
     in[i] = cosf(2 * pi * t * 1) + sinf(2 * pi * t * 2);
     // We are mixing up 1hz & 3hz
   }
+  // Faster fourier transform (we only need to iterate half of the freq.)
+#if 1
+  for (size_t f = 0; f < n / 2; ++f) {
+    out[f] = 0;
+    out[f + n / 2] = 0;
 
-  // We are unmixing them
+    // even indices
+    for (size_t i = 0; i < n; i += 2) {
+      float t = (float)i / n;
+      float complex v = in[i] * cexp(2 * I * pi * t * f);
+      out[f] += v;
+      out[f + n / 2] += v;
+    }
+    // odd indices
+    for (size_t i = 1; i < n; i += 2) {
+      float t = (float)i / n;
+      float complex v = in[i] * cexp(2 * I * pi * t * f);
+      out[f] += v;
+      out[f + n / 2] -= v;
+    }
+  }
+
+#else
+  // Slow fourier transform
   for (size_t f = 0; f < n; ++f) {
     out[f] = 0;
     for (size_t i = 0; i < n; ++i) {
@@ -32,14 +52,16 @@ int main() {
     }
   }
 
+#endif
   for (size_t f = 0; f < n; ++f) {
     printf("%02zu: %.2f, %.2f\n", f, creal(out[f]), cimag(out[f]));
   }
+
   return 0;
 }
-
 // One period of sine wave is 2*pi
 // 1f = 1 hertz
 // if out[f] is positive, it means that freq. is present in the input
 //  the value of it is the amplitude
 // clang -o fft fft.c -lm && ./fft
+
