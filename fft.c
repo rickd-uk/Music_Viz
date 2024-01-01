1c6d607486cb3e8fdd02d2684f6fa90ace0bd3b7
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-02 00:31:57 +0900

    Music Visualizer 2:29:10 - Implements fft correctly

--- fft.c
+++ fft.c
@@ -1,8 +1,46 @@
+#include <assert.h>
 #include <complex.h>
 #include <math.h>
 #include <stdio.h>
+#include <threads.h>
 
 float pi;
+
+void dft(float in[], float complex out[], size_t n) {
+
+  // Slow fourier transform
+  for (size_t f = 0; f < n; ++f) {
+    out[f] = 0;
+    for (size_t i = 0; i < n; ++i) {
+      float t = (float)i / n;
+
+      // use Euler's formula
+      out[f] += in[i] * cexp(2 * I * pi * f * t);
+      /* out_sin[f] += in[i] * sinf(2 * pi * f * t); */
+      /* out_cos[f] += in[i] * cosf(2 * pi * f * t); */
+    }
+  }
+}
+
+void fft(float in[], size_t stride, float complex out[], size_t n) {
+  assert(n > 0);
+
+  if (n == 1) {
+    out[0] = in[0];
+    return;
+  }
+
+  fft(in, stride * 2, out, n / 2);
+  fft(in + stride, stride * 2, out + n / 2, n / 2);
+
+  for (size_t k = 0; k < n / 2; ++k) {
+    float t = (float)k / n;
+    float complex v = cexp(-2 * I * pi * t) * out[k + n / 2];
+    float complex e = out[k];
+    out[k] = e + v;
+    out[k + n / 2] = e - v;
+  }
+}
 
 int main() {
   pi = atan2f(1, 1) * 4;
@@ -17,42 +55,9 @@ int main() {
     // We are mixing up 1hz & 3hz
   }
   // Faster fourier transform (we only need to iterate half of the freq.)
-#if 1
-  for (size_t f = 0; f < n / 2; ++f) {
-    out[f] = 0;
-    out[f + n / 2] = 0;
 
-    // even indices
-    for (size_t i = 0; i < n; i += 2) {
-      float t = (float)i / n;
-      float complex v = in[i] * cexp(2 * I * pi * t * f);
-      out[f] += v;
-      out[f + n / 2] += v;
-    }
-    // odd indices
-    for (size_t i = 1; i < n; i += 2) {
-      float t = (float)i / n;
-      float complex v = in[i] * cexp(2 * I * pi * t * f);
-      out[f] += v;
-      out[f + n / 2] -= v;
-    }
-  }
+  fft(in, 1, out, n);
 
-#else
-  // Slow fourier transform
-  for (size_t f = 0; f < n; ++f) {
-    out[f] = 0;
-    for (size_t i = 0; i < n; ++i) {
-      float t = (float)i / n;
-
-      // use Euler's formula
-      out[f] += in[i] * cexp(2 * I * pi * f * t);
-      /* out_sin[f] += in[i] * sinf(2 * pi * f * t); */
-      /* out_cos[f] += in[i] * cosf(2 * pi * f * t); */
-    }
-  }
-
-#endif
   for (size_t f = 0; f < n; ++f) {
     printf("%02zu: %.2f, %.2f\n", f, creal(out[f]), cimag(out[f]));
   }
@@ -65,3 +70,4 @@ int main() {
 //  the value of it is the amplitude
 // clang -o fft fft.c -lm && ./fft
 
+// Continuee-->  2:13:38 - Music Visualizer (Fast Fourier Transform)
