8b55bc721496818d7caeaa32efa018b8841b5eef
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-15 15:53:57 +0900

    add: shift_args (to handle program file input as param.)

--- src/musicviz.c
+++ src/musicviz.c
@@ -1,13 +1,17 @@
-#include "plug.h"
-
 #include <assert.h>
 #include <complex.h>
 #include <math.h>
-#include <raylib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include <raylib.h>
+
+// posix specific; will not work on Windows
+#include <dlfcn.h>
+
+#include "plug.h"
 
 #define ARRAY_LEN(xs) sizeof(xs) / sizeof(xs[0])
 
@@ -72,15 +76,34 @@ void callback(void *bufferData, unsigned int frames) {
   }
 }
 
+char *shift_args(int *argc, char ***argv) {
+  // check if there are any remaining args
+  assert(*argc > 0);
+  // get the current arg val.
+  char *result = (**argv);
+  (*argv) += 1;
+  (*argc) -= 1;
+  return result;
+}
+
 plug_hello_t plug_hello = NULL;
 
-int main(void) {
+int main(int argc, char **argv) {
 
   plug_hello();
 
   return 0;
 
   pi = atan2f(1, 1) * 4;
+
+  const char *program = shift_args(&argc, &argv);
+
+  if (argc == 0) {
+    fprintf(stderr, "Usage: %s <input>\n", program);
+    fprintf(stderr, "ERROR: no input file is provided\n");
+    return 1;
+  }
+  const char *file_path = shift_args(&argc, &argv);
 
   InitWindow(800, 600, "musicviz");
   SetTargetFPS(60);
