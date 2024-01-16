ae0d7b9bce414fbe510bcd576e5bbad124009cfa
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-16 22:22:29 +0900

    fix: remove *.flac file from music dir

--- src/fft.c
+++ src/fft.c
@@ -22,26 +22,6 @@ void dft(float in[], float complex out[], size_t n) {
   }
 }
 
-void fft(float in[], size_t stride, float complex out[], size_t n) {
-  assert(n > 0);
-
-  if (n == 1) {
-    out[0] = in[0];
-    return;
-  }
-
-  fft(in, stride * 2, out, n / 2);
-  fft(in + stride, stride * 2, out + n / 2, n / 2);
-
-  for (size_t k = 0; k < n / 2; ++k) {
-    float t = (float)k / n;
-    float complex v = cexp(-2 * I * pi * t) * out[k + n / 2];
-    float complex e = out[k];
-    out[k] = e + v;
-    out[k + n / 2] = e - v;
-  }
-}
-
 int main() {
   pi = atan2f(1, 1) * 4;
   /* printf("%.10f\n", pi); */
