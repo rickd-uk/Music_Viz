9b325154f2edc48f5d6e76093a46cb1364ea6dcd
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-23 10:51:26 +0900

    fix: remove unused files

--- build.sh
+++ build.sh
@@ -1,26 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
 
 set -xe
-# Set the project-specific LD_LIBRARY_PATH
+# Print the current working directory
+echo "Current Directory: $(pwd)"
 
-# Set the project-specific LD_LIBRARY_PATH
+# List the files in the current directory
+ls -l
 
-# Get the absolute path of the current directory
-current_dir=$(readlink -f "$(pwd)")
-
-# Disable debugging for the next command
-set +x
-
-# Check if the directory is already in LD_LIBRARY_PATH
-if [[ ":$LD_LIBRARY_PATH:" != *":$current_dir/build:"* ]]; then
-    # If not present, add it to LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH="$current_dir/build:$LD_LIBRARY_PATH"
-else
-    echo "Directory $current_dir/build is already in LD_LIBRARY_PATH."
-fi
-
-# Re-enable debugging for the rest of the script
-set -x
+source .env
 
 
 CFLAGS="-Wall -Wextra -ggdb `pkg-config --cflags raylib`"
@@ -30,11 +18,9 @@ mkdir -p ./build
 # -fPIC: position independent code (does not depend on absolute addr. in memory)
 clang $CFLAGS -o ./build/libplug.so -fPIC -shared ./src/plug.c $LIBS
 
-clang $CFLAGS -DHOTRELOAD -o ./build/musicviz  ./src/musicviz.c $LIBS -L./build/ 
-# clang $CFLAGS -DHOTRELOAD -o ./build/musicviz ./src/musicviz.c $LIBS -L./build/ -Wl,-rpath,'$ORIGIN/build'
-# clang $CFLAGS -o ./build/musicviz ./src/plug.c ./src/musicviz.c $LIBS -L./build/ 
+clang $CFLAGS -DHOTRELOAD -o ./build/musicviz ./src/musicviz.c $LIBS -L./build/ 
 clang -o ./build/fft ./src/fft.c -lm
 
 # Run the program with the specified arguments
 # ./build/musicviz "$@"
-
+set +x
