c50c89e8a9cb312ca49fb10ed521705824421ef7
Rick D <78909973+rickd-uk@users.noreply.github.com>
2024-01-30 15:57:42 +0900

    add: use a simple easing function to make circles/sticks more visible  41:00

--- src/plug.c
+++ src/plug.c
@@ -197,8 +197,8 @@ void plug_update(void) {
                         // height
                         h};
       // DrawRectangle(i * cell_width, h - y, ceilf(cell_width), y, color);
-      float thickness = cell_width / 2 *t;
-      float radius = cell_width * t;
+      float thickness = cell_width / 2 * sqrtf(t);
+      float radius = cell_width * sqrtf(t);
       DrawCircleV(startPos, radius, color);
       DrawLineEx(startPos, endPos, thickness, color);
     }
